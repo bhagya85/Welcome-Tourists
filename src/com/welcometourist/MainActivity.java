6dd7229e4d0e130fd1408903b777f3b01ba3471e
bhagyalakshmi.y@gmail.com <bhagyalakshmi.y@gmail.com>
2012-07-12 17:35:30 -0700

    Text box

--- src/com/welcometourist/MainActivity.java
+++ src/com/welcometourist/MainActivity.java
@@ -1,10 +1,13 @@
 package com.welcometourist;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Toast;
+ 
 
 public class MainActivity extends Activity {
 
@@ -12,13 +15,42 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addKeyListener();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+    public void addKeyListener() {
+    	 
+    	// get edittext component
+    final EditText edittext = (EditText) findViewById(R.id.editText);
+     
+    	// add a keylistener to keep track user input
+    	edittext.setOnKeyListener(new OnKeyListener() {
+    	public boolean onKey(View v, int keyCode, KeyEvent event) {
+     
+    		// if keydown and "enter" is pressed
+    		if ((event.getAction() == KeyEvent.ACTION_DOWN)
+    			&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+     
+    			// display a floating message
+    			Toast.makeText(MainActivity.this,
+    		//	final EditText = (EditText) findViewById(R.id.editText)
+    			edittext.getText(), Toast.LENGTH_LONG).show();
+    			return true;
+     
+    		} else if ((event.getAction() == KeyEvent.ACTION_DOWN)
+    			&& (keyCode == KeyEvent.KEYCODE_9)) {
+     
+    			// display a floating message
+    			Toast.makeText(MainActivity.this,
+    				"Number 9 is pressed!", Toast.LENGTH_LONG).show();
+    			return true;
+    		}
+     
+    		return false;
+    	}
+     });
 
     
+}
 }
